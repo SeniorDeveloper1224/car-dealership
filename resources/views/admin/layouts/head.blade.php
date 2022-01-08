e154baa7398e8a860b6b4385d291c1f6ff126040
YoshidaYutaka1224 <52586904+YoshidaYutaka1224@users.noreply.github.com>
2022-01-08 16:15:53 +0900

    update for icon

--- resources/views/admin/layouts/head.blade.php
+++ resources/views/admin/layouts/head.blade.php
@@ -6,13 +6,13 @@
         <meta name="JMNO" content="xFusion">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta name="description" content="An amazing CRM for xFusion">
-    
+
         <!-- Fav Icon  -->
         <link rel="shortcut icon" href="{{ asset('./admin/images/favicon.png ') }}">
 
         <!-- Page Title  -->
-        <title>xFusion | Dashboard</title>
-        
+        <title>Car Dealer | Dashboard</title>
+
         <!-- StyleSheets  -->
         <link rel="stylesheet" href= "{{ asset('./admin/assets/css/dashlite.css') }}" >
         <link id="skin-default" rel="stylesheet" href="{{ asset('./admin/assets/css/skins/theme-green.css') }}">
